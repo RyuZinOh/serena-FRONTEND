7a07560f4b4d74a7a48cd9a67f139535d20aa163
Safal Lama <140276215+RyuZinOh@users.noreply.github.com>
2024-11-26 23:14:44 +0545

    trying to revamp design

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Register from "./components/Register";
-import Login from "./components/Login";
-import ForgetPassword from "./components/ForgetPassword";
-import AdminDashboard from "./components/Dashboard/AdminDashboard";
-import UserDashboard from "./components/Dashboard/UserDashboard";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Register />} /> {/* Register page at root */}
-        <Route path="/login" element={<Login />} /> {/* Login page */}
-        <Route path="/register" element={<Register />} /> {/* Register page */}
-        {/* Forget Password page */}
-        <Route path="/forgetpass" element={<ForgetPassword />} />{" "}
-        {/* Dashboard Routes */}
-        <Route path="/user-dashboard" element={<UserDashboard />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
-      </Routes>
+      <Routes></Routes>
     </Router>
   );
 };
