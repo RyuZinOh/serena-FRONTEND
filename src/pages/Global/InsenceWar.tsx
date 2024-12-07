173a7d266636f990a2c63101ca54be95b8922cfa
ryuzinoh <whysafalisop@gmail.com>
2024-12-07 23:12:16 +0545

    changed path again

--- src/pages/Global/InsenceWar.tsx
+++ src/pages/Global/InsenceWar.tsx
@@ -238,7 +238,7 @@ const IncenseWar: React.FC = () => {
               >
                 <div className="flex justify-between items-center mb-4">
                   <img
-                    src="/src/logo.png"
+                    src="/assets/logo.png"
                     alt="Incense War Logo"
                     className="w-12 h-12 rounded-full"
                   />
